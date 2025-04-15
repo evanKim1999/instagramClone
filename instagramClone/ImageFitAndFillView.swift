b0a8f0ec33ff699ed54179aea7f3cdfe0ad1e84e
evan_kim <zone227@naver.com>
2025-04-15 23:39:20 +0900

    chapter 1 - 포스트 뷰

---
+++ instagramClone/ImageFitAndFillView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageFitAndFillView.swift
+//  instagramClone
+//
+//  Created by eunchanKim on 4/15/25.
+//
+
+import SwiftUI
+
+struct ImageFitAndFillView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageFitAndFillView()
+}
---
+++ instagramClone/MainTapView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTapView.swift
+//  instagramClone
+//
+//  Created by eunchanKim on 4/15/25.
+//
+
+import SwiftUI
+
+struct MainTapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTapView()
+}
