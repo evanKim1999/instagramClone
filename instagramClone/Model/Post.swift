1191c21c4851807dba47c0aa1e00dcba34aa35a6
evan_kim <zone227@naver.com>
2025-04-16 20:05:42 +0900

    chapter 2~4 - Firebase 연동

---
+++ instagramClone/Model/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  instagramClone
+//
+//  Created by eunchanKim on 4/16/25.
+//
+
+import Foundation
---
+++ instagramClone/NewPost/NewPostViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NewPostViewModel.swift
+//  instagramClone
+//
+//  Created by eunchanKim on 4/16/25.
+//
+
+import Foundation
